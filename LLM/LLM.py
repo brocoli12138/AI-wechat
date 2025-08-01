b2d18bd401699d74c4c63183347c348b81a4ef4e
brocoli12138 <3114563956@qq.com>
2025-08-01 16:04:18 +0800

    a reformed designed for this agent

--- LLM/LLM.py
+++ LLM/LLM.py
@@ -1,12 +1,10 @@
 from ..config import Config
 from ..tools.tools_manager import ToolManager
 from openai import OpenAI
-from typing import List, Dict, Any
-from openai.types.chat import ChatCompletionMessageParam
 
 class Responsor:
     def __init__(self, config: Config):
         self.tool_manager = ToolManager()
         self.openai_client = OpenAI(api_key=config.openai_key,
                                     base_url=config.openai_endpoint)
-        self.temp_context: List[ChatCompletionMessageParam] = []
+        self.temp_context = []
